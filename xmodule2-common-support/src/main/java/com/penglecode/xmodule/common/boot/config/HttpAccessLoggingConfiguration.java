c16791210f131b341e4e9b18c8a101233c76c32d
pengp <pengp@certusnet.com.cn>
2019-11-12 15:57:56 +0800

    commit

--- xmodule2-common-support/src/main/java/com/penglecode/xmodule/common/boot/config/HttpAccessLoggingConfiguration.java
+++ xmodule2-common-support/src/main/java/com/penglecode/xmodule/common/boot/config/HttpAccessLoggingConfiguration.java
@@ -3,6 +3,8 @@ package com.penglecode.xmodule.common.boot.config;
 import javax.servlet.Filter;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +13,7 @@ import com.penglecode.xmodule.common.web.filter.AbstractHttpAccessLoggingFilter;
 import com.penglecode.xmodule.common.web.support.HttpAccessLogDAO;
 
 @Configuration
+@ConditionalOnWebApplication(type = Type.SERVLET)
 @ConditionalOnBean({HttpAccessLogDAO.class, AbstractHttpAccessLoggingFilter.class})
 public class HttpAccessLoggingConfiguration extends AbstractSpringConfiguration {
 	
